edc435453747f05084679287f841cde26df4c718
alexharris <alexharris6@gmail.com>
2019-11-11 16:23:27 -0800

    add fathrom tracker

--- src/App.vue
+++ src/App.vue
@@ -93,24 +93,7 @@ export default {
     }
   },
   mounted() {
-    this.buildCanvas()
-
-
-
-    // Fathom - simple website analytics - https://usefathom.com
-    (function(f, a, t, h, o, m){
-    a[h]=a[h]||function(){
-    (a[h].q=a[h].q||[]).push(arguments)
-    };
-    o=f.createElement('script'),
-    m=f.getElementsByTagName('script')[0];
-    o.async=1; o.src=t; o.id='fathom-script';
-    m.parentNode.insertBefore(o,m)
-    })(document, window, 'https://cdn.usefathom.com/tracker.js', 'fathom');
-    fathom('set', 'siteId', 'DIZWQBUC');
-    fathom('trackPageview');
-    
-    // Fathom       
+    this.buildCanvas()     
   },
   methods: {
     buildCanvas() {
